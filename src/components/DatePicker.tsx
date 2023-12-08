1b206dd4568a8e7964deec28848be5daa6a4ff96
PapaniaP <papaniap11@gmail.com>
2023-12-08 11:03:52 +0100

    date package install

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,53 +1,63 @@
-import React, { useState } from 'react';
-import { IonDatetime, IonItem, IonLabel, IonButton, IonContent, IonModal, IonCol, IonGrid, IonRow } from '@ionic/react';
-import { formatISO, startOfToday, addDays } from 'date-fns';
+import React, { useState } from "react";
+import {
+	IonDatetime,
+	IonItem,
+	IonLabel,
+	IonButton,
+	IonContent,
+	IonModal,
+	IonCol,
+	IonGrid,
+	IonRow,
+} from "@ionic/react";
+import { formatISO, startOfToday, addDays } from "date-fns";
 
 const DatePicker: React.FC = () => {
-  const [selectedDate, setSelectedDate] = useState<string>(formatISO(startOfToday()));
-  const [showPicker, setShowPicker] = useState<boolean>(false);
+	const [selectedDate, setSelectedDate] = useState<string>(formatISO(startOfToday()));
+	const [showPicker, setShowPicker] = useState<boolean>(false);
 
-  const minDate = formatISO(startOfToday());
-  const maxDate = formatISO(addDays(startOfToday(), 7));
+	const minDate = formatISO(startOfToday());
+	const maxDate = formatISO(addDays(startOfToday(), 7));
 
-  const handleDateChange = (e: CustomEvent) => {
-    setSelectedDate(e.detail.value as string);
-    setShowPicker(false);  // Automatically close the picker when a date is selected
-  };
+	const handleDateChange = (e: CustomEvent) => {
+		setSelectedDate(e.detail.value as string);
+		setShowPicker(false); // Automatically close the picker when a date is selected
+	};
 
-  return (
-    <IonContent className="ion-padding">
-      <IonGrid>
-         <IonRow><p>PLease pick a date:</p></IonRow>
-          <IonRow>      
-            <IonButton onClick={() => setShowPicker(true)}>{selectedDate}</IonButton>
-            <IonModal isOpen={showPicker} onDidDismiss={() => setShowPicker(false)}>
-        <IonItem>
-          <IonLabel>Pick a Day</IonLabel>
-          <IonDatetime
-            presentation="date"
-            min={minDate}
-            max={maxDate}
-            value={selectedDate}
-            onIonChange={handleDateChange}
-          />
-        </IonItem>
-        {/* <IonButton onClick={() => setShowPicker(false)}>Done</IonButton> */}
-      </IonModal>
-</IonRow>
- {/* <IonRow>
+	return (
+		<IonContent className="ion-padding">
+			<IonGrid>
+				<IonRow>
+					<p>PLease pick a date:</p>
+				</IonRow>
+				<IonRow>
+					<IonButton onClick={() => setShowPicker(true)}>{selectedDate}</IonButton>
+					<IonModal
+						isOpen={showPicker}
+						onDidDismiss={() => setShowPicker(false)}
+					>
+						<IonItem>
+							<IonLabel>Pick a Day</IonLabel>
+							<IonDatetime
+								presentation="date"
+								min={minDate}
+								max={maxDate}
+								value={selectedDate}
+								onIonChange={handleDateChange}
+							/>
+						</IonItem>
+						{/* <IonButton onClick={() => setShowPicker(false)}>Done</IonButton> */}
+					</IonModal>
+				</IonRow>
+				{/* <IonRow>
   <IonCol>
      <IonRow><p>Washing machine 1</p></IonRow>
      <IonRow></IonRow>
   </IonCol>
  </IonRow> */}
-      </IonGrid>      
-    </IonContent>
-  );
+			</IonGrid>
+		</IonContent>
+	);
 };
 
 export default DatePicker;
-
-
-
-
-
