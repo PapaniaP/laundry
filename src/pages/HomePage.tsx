d168f2d3df160bbf8b57f6168f9994ab5260f28d
Gansey14 <hlousek.samuel14@gmail.com>
2023-12-09 13:38:45 +0100

    All time slots added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,12 @@
 import {
-	IonPage,
-	IonHeader,
-	IonToolbar,
-	IonButtons,
-	IonMenuButton,
-	IonTitle,
-	IonContent,
-	IonButton, IonDatetime, IonItem, IonLabel, IonModal, IonGrid, IonRow
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonTitle,
+  IonContent,
+  IonButton, IonDatetime, IonItem, IonLabel, IonModal, IonGrid, IonRow, IonCol
 } from "@ionic/react";
 import { useParams } from "react-router";
 import "./HomePage.css";
@@ -18,15 +18,15 @@ import { db } from "../firebase-config";
 import { formatISO, startOfToday, addDays } from 'date-fns';
 
 interface Booking {
-	uid: string;
-bookedTimes: number[];
+  uid: string;
+  bookedTimes: number[];
 }
 
 function HomePage() {
   const [selectedValues, setSelectedValues] = useState<number[]>([]);
   const [Booking, setBooking] = useState<Booking>({
-	uid:"",
-	bookedTimes: []
+    uid: "",
+    bookedTimes: []
   });
 
   const [selectedDate, setSelectedDate] = useState<string>(formatISO(startOfToday()));
@@ -42,12 +42,12 @@ function HomePage() {
 
   const dateToBeFetched = selectedDate.split("T")[0];
 
-    useEffect(() => {
+  useEffect(() => {
     localStorage.setItem('selectedDate', dateToBeFetched);
   }, [dateToBeFetched]);
 
-//   const datesCollectionRef = collection(db, "building-1" , dateToBeFetched)
-const dateDocRef = doc(db, "building-1", dateToBeFetched);
+  //   const datesCollectionRef = collection(db, "building-1" , dateToBeFetched)
+  const dateDocRef = doc(db, "building-1", dateToBeFetched);
 
   const handleButtonClick = (value: number) => {
     setSelectedValues(prev => {
@@ -66,33 +66,33 @@ const dateDocRef = doc(db, "building-1", dateToBeFetched);
   useEffect(() => {
     console.log(selectedValues);
   }, [selectedValues]);
-  
-const handleBooking = async (event: React.FormEvent) => {
+
+  const handleBooking = async (event: React.FormEvent) => {
     event.preventDefault(); // Prevent default form submission behavior
 
     try {
-        // Prepare the booking data
-        const newBooking = {
-            uid: "some-uid", // Replace with actual UID
-            bookedTimes: selectedValues
-        };
+      // Prepare the booking data
+      const newBooking = {
+        uid: "some-uid", // Replace with actual UID
+        bookedTimes: selectedValues
+      };
 
-        // Update the document with the new booking
-        await updateDoc(dateDocRef, {
-            bookings: arrayUnion(newBooking)
-        });
+      // Update the document with the new booking
+      await updateDoc(dateDocRef, {
+        bookings: arrayUnion(newBooking)
+      });
 
-        // Reset the booking state if needed
-        setBooking({
-            uid: "",
-            bookedTimes: []
-        });
+      // Reset the booking state if needed
+      setBooking({
+        uid: "",
+        bookedTimes: []
+      });
 
-        // Possibly redirect or show a success message
+      // Possibly redirect or show a success message
     } catch (error) {
-        console.error("Failed to create booking:", error);
+      console.error("Failed to create booking:", error);
     }
-};
+  };
 
   return (
     <IonPage>
@@ -106,50 +106,239 @@ const handleBooking = async (event: React.FormEvent) => {
       </IonHeader>
       <IonContent fullscreen className="ion-padding">
         <form onSubmit={handleBooking}>
-		<IonGrid>
-         <IonRow><p>Please pick a date:</p></IonRow>
-          <IonRow>      
-            <IonButton onClick={() => setShowPicker(true)}>{selectedDate}</IonButton>
-            <IonModal isOpen={showPicker} onDidDismiss={() => setShowPicker(false)}>
-        <IonItem>
-          <IonLabel>Pick a Day</IonLabel>
-          <IonDatetime
-            presentation="date"
-            min={minDate}
-            max={maxDate}
-            value={selectedDate}
-            onIonChange={handleDateChange}
-          />
-        </IonItem>
-        {/* <IonButton onClick={() => setShowPicker(false)}>Done</IonButton> */}
-      </IonModal>
-</IonRow>
+          <IonGrid>
+            <IonRow><p>Please pick a date:</p></IonRow>
+            <IonRow>
+              <IonButton onClick={() => setShowPicker(true)}>{selectedDate}</IonButton>
+              <IonModal isOpen={showPicker} onDidDismiss={() => setShowPicker(false)}>
+                <IonItem>
+                  <IonLabel>Pick a Day</IonLabel>
+                  <IonDatetime
+                    presentation="date"
+                    min={minDate}
+                    max={maxDate}
+                    value={selectedDate}
+                    onIonChange={handleDateChange}
+                  />
+                </IonItem>
+                {/* <IonButton onClick={() => setShowPicker(false)}>Done</IonButton> */}
+              </IonModal>
+            </IonRow>
 
-      </IonGrid>      
+          </IonGrid>
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonRow> Washer 1</IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={1}
+                    text="07:00 - 09:00"
+                    value={1}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(1)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={2}
+                    text="09:00 - 11:00"
+                    value={2}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(2)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={3}
+                    text="11:00 - 13:00"
+                    value={3}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(3)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={4}
+                    text="13:00 - 15:00" // Updated text for id 4
+                    value={4}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(4)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={5}
+                    text="15:00 - 17:00"
+                    value={5}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(5)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={6}
+                    text="17:00 - 19:00"
+                    value={6}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(6)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={7}
+                    text="19:00 - 21:00"
+                    value={7}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(7)}
+                  />
+                </IonRow>
+              </IonCol>
 
-        <TimeButton
-		id={1}
-          text="7:00 - 9:00"
-          value={1}
-          onButtonClick={handleButtonClick}
-          isSelected={selectedValues.includes(1)} // Check for value 1
-        />
-        <TimeButton
-		id={2}
-          text="9:00 - 11:00"
-          value={2}
-          onButtonClick={handleButtonClick}
-          isSelected={selectedValues.includes(2)} // Check for value 2, not 1
-        />
+              <IonCol>
+                <IonRow> Washer 2</IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={8}
+                    text="07:00 - 09:00"
+                    value={8}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(8)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={9}
+                    text="09:00 - 11:00"
+                    value={9}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(9)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={10}
+                    text="11:00 - 13:00"
+                    value={10}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(10)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={11}
+                    text="13:00 - 15:00"
+                    value={11}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(11)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={12}
+                    text="15:00 - 17:00"
+                    value={12}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(12)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={13}
+                    text="17:00 - 19:00"
+                    value={13}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(13)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={14}
+                    text="19:00 - 21:00"
+                    value={14}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(14)}
+                  />
+                </IonRow>
+              </IonCol>
 
-		<IonButton
-			expand="block"
-			// slot="end"
-			type="submit"
-		>
-			Save and Create
-		</IonButton>
-		</form>
+              <IonCol>
+                <IonRow> Dryer</IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={15}
+                    text="07:00 - 09:00"
+                    value={15}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(15)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={16}
+                    text="09:00 - 11:00"
+                    value={16}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(16)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={17}
+                    text="11:00 - 13:00"
+                    value={17}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(17)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={18}
+                    text="13:00 - 15:00"
+                    value={18}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(18)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={19}
+                    text="15:00 - 17:00"
+                    value={19}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(19)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={20}
+                    text="17:00 - 19:00"
+                    value={20}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(20)}
+                  />
+                </IonRow>
+                <IonRow>
+                  <TimeButton
+                    id={21}
+                    text="19:00 - 21:00"
+                    value={21}
+                    onButtonClick={handleButtonClick}
+                    isSelected={selectedValues.includes(21)}
+                  />
+                </IonRow>
+              </IonCol>
+
+            </IonRow>
+          </IonGrid>
+          <IonButton
+            expand="block"
+            // slot="end"
+            type="submit"
+          >
+            Save and Create
+          </IonButton>
+        </form>
       </IonContent>
     </IonPage>
   );
