2b321895cfe6388d890b61fb2d11ad5543e1ea4a
Gansey14 <hlousek.samuel14@gmail.com>
2023-12-11 20:45:53 +0100

    deleting times functionality done

--- src/pages/YourBookingsPage.tsx
+++ src/pages/YourBookingsPage.tsx
@@ -1,9 +1,9 @@
-import { IonContent, IonPage, IonHeader, IonToolbar, IonButtons, IonTitle, IonMenuButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonLabel } from "@ionic/react";
+import { IonContent, IonPage, IonHeader, IonToolbar, IonButtons, IonTitle, IonMenuButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonLabel, IonButton } from "@ionic/react";
 import { useState, useEffect } from "react";
-import { Booking } from "./HomePage";
-import { collection, onSnapshot } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { collection, onSnapshot, doc, updateDoc, arrayRemove, arrayUnion } from "firebase/firestore";
 import { db } from "../firebase-config";
-import { getAuth } from "firebase/auth";
+import { Booking } from "./HomePage";
 
 interface BookingsByDate {
     [date: string]: Booking[];
@@ -13,38 +13,98 @@ const YourBookingsPage: React.FC = () => {
     const [bookingsByDate, setBookingsByDate] = useState<BookingsByDate>({});
     const [loading, setLoading] = useState(true); // Loading state
     const auth = getAuth();
-    const currentUser = auth.currentUser?.uid;
 
     useEffect(() => {
-        if (currentUser) {
-            setLoading(true); // Begin loading
-            const buildingCollectionRef = collection(db, "building-1");
+        const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setLoading(true); // Begin loading
+                const buildingCollectionRef = collection(db, "building-1");
 
-            const unsubscribe = onSnapshot(buildingCollectionRef, (snapshot) => {
-                const newBookingsByDate: BookingsByDate = {};
+                const unsubscribe = onSnapshot(buildingCollectionRef, (snapshot) => {
+                    const newBookingsByDate: BookingsByDate = {};
 
-                snapshot.docs.forEach((doc) => {
-                    const date = doc.id;
-                    const allBookings: Booking[] = doc.data().bookings || [];
-                    const userBookings = allBookings.filter(booking => booking.uid === currentUser);
+                    snapshot.docs.forEach((doc) => {
+                        const date = doc.id;
+                        const allBookings: Booking[] = doc.data().bookings || [];
+                        // Filter user's bookings and sort the bookedTimes
+                        const userBookings = allBookings
+                            .filter(booking => booking.uid === user.uid)
+                            .map(booking => ({
+                                ...booking,
+                                bookedTimes: booking.bookedTimes.slice().sort((a, b) => a - b)
+                            }));
 
-                    if (userBookings.length > 0) {
-                        newBookingsByDate[date] = userBookings;
-                    }
+                        if (userBookings.length > 0) {
+                            newBookingsByDate[date] = userBookings;
+                        }
+                    });
+
+                    console.log("Sorted bookings by date:", newBookingsByDate); // Debug: Log sorted bookings
+                    setBookingsByDate(newBookingsByDate);
+                    setLoading(false); // End loading
+                }, (error) => {
+                    console.error("Error listening to the collection:", error);
+                    setLoading(false); // End loading even if there's an error
                 });
 
-                setBookingsByDate(newBookingsByDate);
-                setLoading(false); // End loading
-            }, (error) => {
-                console.error("Error listening to the collection:", error);
-                setLoading(false); // End loading even if there's an error
-            });
+                return () => unsubscribe();
+            } else {
+                setLoading(false);
+            }
+        });
 
-            return () => unsubscribe();
-        } else {
-            setLoading(false);
+        return () => unsubscribeAuth();
+    }, [auth]);
+
+    const deleteBooking = async (date: string, bookingTime: number) => {
+        const bookingDocRef = doc(db, "building-1", date);
+        const currentBooking = bookingsByDate[date].find(booking => booking.bookedTimes.includes(bookingTime));
+
+        if (currentBooking) {
+            // If it's the last number in the array or if the array only contains the number being deleted
+            if (currentBooking.bookedTimes.length === 1 || (currentBooking.bookedTimes.length > 1 && currentBooking.bookedTimes.every(bt => bt === bookingTime))) {
+                // Remove the whole booking object from the Firestore document
+                await updateDoc(bookingDocRef, {
+                    bookings: arrayRemove(currentBooking)
+                });
+            } else {
+                // Create a copy of the booking without the deleted time
+                const updatedBooking = {
+                    ...currentBooking,
+                    bookedTimes: currentBooking.bookedTimes.filter(time => time !== bookingTime)
+                };
+                // First remove the old booking object
+                await updateDoc(bookingDocRef, {
+                    bookings: arrayRemove(currentBooking)
+                });
+                // Then add the updated booking object
+                await updateDoc(bookingDocRef, {
+                    bookings: arrayUnion(updatedBooking)
+                });
+            }
+
+            // Update the state to reflect the deletion
+            setBookingsByDate(prevState => {
+                const updatedBookings = { ...prevState };
+                const currentBookings = updatedBookings[date];
+
+                if (currentBookings) {
+                    // Filter out the booking that contains the time to be deleted
+                    const remainingBookings = currentBookings.filter(booking => !booking.bookedTimes.includes(bookingTime));
+
+                    if (remainingBookings.length > 0) {
+                        // If there are remaining bookings, update the date entry
+                        updatedBookings[date] = remainingBookings;
+                    } else {
+                        // If all bookings for the date have been deleted, remove the date entry
+                        delete updatedBookings[date];
+                    }
+                }
+
+                return updatedBookings;
+            });
         }
-    }, [currentUser]);
+    };
 
     if (loading) {
         return <IonPage><IonContent>Loading your bookings...</IonContent></IonPage>;
@@ -61,26 +121,37 @@ const YourBookingsPage: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
-                {Object.entries(bookingsByDate).map(([date, bookings], index) => (
-                    <IonCard key={index}>
-                        <IonCardHeader>
-                            <IonCardTitle>{date}</IonCardTitle>
-                        </IonCardHeader>
-                        <IonCardContent>
-                            {bookings.map((booking, bookingIndex) => (
-                                <IonItem key={bookingIndex}>
-                                    <IonLabel>
-                                        Booking Times: {booking.bookedTimes.join(', ')}
-                                    </IonLabel>
-                                </IonItem>
-                            ))}
-                        </IonCardContent>
-                    </IonCard>
-                ))}
+                {Object.entries(bookingsByDate).length > 0 ? (
+                    Object.entries(bookingsByDate).map(([date, bookings], index) => (
+                        <IonCard key={index}>
+                            <IonCardHeader>
+                                <IonCardTitle>{date}</IonCardTitle>
+                            </IonCardHeader>
+                            <IonCardContent>
+                                {bookings.map((booking, bookingIndex) => (
+                                    booking.bookedTimes.map((time, timeIndex) => (
+                                        <IonItem key={`${bookingIndex}-${timeIndex}`}>
+                                            <IonLabel>
+                                                Booking Time: {time}
+                                            </IonLabel>
+                                            <IonButton
+                                                onClick={() => deleteBooking(date, time)}
+                                                color="danger"
+                                            >
+                                                Delete
+                                            </IonButton>
+                                        </IonItem>
+                                    ))
+                                ))}
+                            </IonCardContent>
+                        </IonCard>
+                    ))
+                ) : (
+                    <IonItem>No bookings found.</IonItem>
+                )}
             </IonContent>
         </IonPage>
     );
 };
 
 export default YourBookingsPage;
-``
